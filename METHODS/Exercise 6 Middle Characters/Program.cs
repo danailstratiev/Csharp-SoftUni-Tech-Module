ae58346a390e39f06fb094256ae27a110594fd5e
danailstratirv <stratyd@abv.bg>
2018-12-30 00:21:36 +0200

    Middle Characters

---
+++ METHODS/Exercise 6 Middle Characters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Exercise_6_Middle_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+
+            if (word.Length % 2 == 0)
+            {
+                Console.WriteLine($"{word[word.Length/2 -1]}{word[word.Length/2]}");
+            }
+            else
+            {
+                Console.WriteLine($"{word[word.Length/2]}");
+            }
+
+        }
+    }
+}
