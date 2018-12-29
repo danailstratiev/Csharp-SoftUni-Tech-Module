055e72f52fdd7c430cb09bfd8e63453ae2f1580a
danailstratirv <stratyd@abv.bg>
2018-12-30 01:20:24 +0200

    Geometry Calculator

---
+++ METHODS/More Exercises Geometry Calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _4._11_Geometry_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+            switch (figure)
+            {
+                case "triangle":
+                    Triangle();
+                    break;
+                case "square":
+                    Square();
+                    break;
+                case "circle":
+                    Circle();
+                    break;
+                case "rectangle":
+                    Rectangle();
+                    break;
+
+            }
+        }
+        static void Triangle ()
+        {
+            double side = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{side*height/2:F2}");
+        }
+        static void Square()
+        {
+            double side = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{Math.Pow(side,2):F2}");
+        }
+        static void Circle()
+        {
+            double side = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{Math.PI*side*side:F2}");
+        }
+        static void Rectangle()
+        {
+            double sideA = double.Parse(Console.ReadLine());
+            double sideB = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{sideA*sideB:F2}");
+        }
+
+    }
+}
