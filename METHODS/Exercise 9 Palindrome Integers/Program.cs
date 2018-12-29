f9ae0344a134991748b2134c5cba6ae155f68b83
danailstratirv <stratyd@abv.bg>
2018-12-30 00:24:25 +0200

    Palindrome Integers

---
+++ METHODS/Exercise 9 Palindrome Integers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Exercise_9_Palindrome_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            bool isPalindrome = false;
+
+            while (n != "END")
+            {
+                char[] nums = n.ToString().ToArray();
+
+                int newNum = int.Parse(string.Join("", nums.Reverse()));
+
+                if (newNum == int.Parse(n))
+                {
+                    isPalindrome = true;
+                }
+                else
+                {
+                    isPalindrome = false;
+                }
+                if (isPalindrome == true)
+                {
+                Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+                n = Console.ReadLine();
+            }
+        }
+    }
+}
