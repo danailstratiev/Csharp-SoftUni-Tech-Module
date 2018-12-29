2ac7f2b720eee3f4c140374f5099cb28be0bd8bc
danailstratirv <stratyd@abv.bg>
2018-12-30 00:20:47 +0200

    Add and Subtract

---
+++ METHODS/Exercise 5 Add and Subtract/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Exercise_5_Add_and_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine($"{numbers[0] + numbers[1] - numbers[2]}");
+
+        }
+    }
+}
