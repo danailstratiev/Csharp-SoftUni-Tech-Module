7ebeabe8b26f8d81d0c7f3317d4116abbcb167da
danailstratirv <stratyd@abv.bg>
2018-12-29 23:11:15 +0200

    Lower or upper

---
+++ DATA TYPES AND VARIABLES/TechFund Data types p07 Lower or upper/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TechFund_Data_types_p07_Lower_or_upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = char.Parse(Console.ReadLine());
+
+            for (char i = 'a'; i <= 'z' ; i++)
+            {
+                if (letter == i)
+                {
+            Console.WriteLine("lower-case");                    
+                }
+            }
+            for (char i = 'A'; i <= 'Z'; i++)
+            {
+                if (letter == i)
+                {
+                    Console.WriteLine("upper-case");
+                }
+            }
+
+        }
+    }
+}
