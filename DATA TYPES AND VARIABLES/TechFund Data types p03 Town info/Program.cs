c6f02ddf27e3ba2c58e545618dc471d41c61f644
danailstratirv <stratyd@abv.bg>
2018-12-29 23:05:20 +0200

    Town info

---
+++ DATA TYPES AND VARIABLES/TechFund Data types p03 Town info/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TechFund_Data_types_p03_Town_info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            long population = long.Parse(Console.ReadLine());
+            long area = long.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Town { town } has population of {population} and area {area} square km.");
+        }
+    }
+}
