0752a6773100acf61ea4422f7c42c178a2634ab1
danailstratirv <stratyd@abv.bg>
2018-12-30 01:00:36 +0200

    Numbers in reversed order

---
+++ METHODS/More Exercises Numbers in reversed order/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _4._4_Numbers_in_reversed_order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            Console.WriteLine(ReversedNumber(number));
+        }
+        static string ReversedNumber(string number)
+        {
+            string reversedNumber = "";
+            for (int i = number.Length-1;  i >= 0 ;  i--)
+            {
+                reversedNumber += number[i];
+            }
+
+            return reversedNumber;
+        }
+
+    }
+}
