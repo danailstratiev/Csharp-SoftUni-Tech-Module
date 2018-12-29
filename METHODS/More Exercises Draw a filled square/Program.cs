781111283c482a95fa7172702044ddb03d3b402a
danailstratirv <stratyd@abv.bg>
2018-12-30 00:50:38 +0200

    Draw a filled square

---
+++ METHODS/More Exercises Draw a filled square/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _3._4_Lab_Draw_a_filled_square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintDashes(n);
+            PrintBody(n);
+            PrintDashes(n);
+            
+        }
+        static void PrintDashes (int n)
+        {
+            string dashes = new string('-', 2 * n);
+            Console.WriteLine(dashes);
+        }
+        static void PrintBody (int n)
+        {
+            for (int i = 0; i < n-2; i++)
+            {
+                Console.Write('-');
+                for (int j = 0; j < n-1; j++)
+                {
+                    Console.Write("\\/");
+                }
+                Console.WriteLine('-');
+            }
+        }
+    }
+}
