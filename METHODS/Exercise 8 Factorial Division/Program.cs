24f21886f8fb298ffe6153b55dd08992dadba613
danailstratirv <stratyd@abv.bg>
2018-12-30 00:23:11 +0200

    Factorial Division

---
+++ METHODS/Exercise 8 Factorial Division/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise_8_Factorial_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            decimal factorialA = 1.00m;
+
+            for (int i = 1; i <= a; i++)
+            {
+                factorialA *= i;
+            }
+
+            decimal factorialB = 1.00m;
+
+            for (int i = 1; i <= b; i++)
+            {
+                factorialB *= i;
+            }
+
+            decimal result = factorialA / factorialB;
+
+            Console.WriteLine($"{result:F2}");
+        }
+        
+    }
+}
