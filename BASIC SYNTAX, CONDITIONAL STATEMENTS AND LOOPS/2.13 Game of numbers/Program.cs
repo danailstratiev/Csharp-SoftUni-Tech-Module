a0a9958e0af62e40ab866ddcfa27c77b717dddf0
danailstratirv <stratyd@abv.bg>
2018-12-29 22:54:16 +0200

    Game of numbers

---
+++ BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/2.13 Game of numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _2._13_Game_of_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int magicNum = int.Parse(Console.ReadLine());
+            int counter = 0;
+            int sum = 0;
+
+            for (int i = n ; i >= m; i--)
+            {
+                for (int j = n; j >= m ; j--)
+                {
+                   
+                    if (i + j == magicNum)
+                    {
+                        Console.WriteLine($"Number found! {i} + {j} = {magicNum}");
+                        return;
+                    }
+                    counter++;
+
+                }
+            }
+            Console.WriteLine($"{counter} combinations - neither equals 100");
+        }
+    }
+}
