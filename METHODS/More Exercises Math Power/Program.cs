0c77e63c5cfe9e16f71b1b441d8c3b31953c488d
danailstratirv <stratyd@abv.bg>
2018-12-30 00:53:54 +0200

    Math Power

---
+++ METHODS/More Exercises Math Power/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _4._7_Lab_Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            var power = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Power(number,power));
+        }
+        static double Power (int number, int power)
+        {
+            var result = 1.0;
+
+            for (int i = 0; i < power; i++)
+            {
+                result *= number;
+
+            }
+            return result;
+        }
+    }
+}
