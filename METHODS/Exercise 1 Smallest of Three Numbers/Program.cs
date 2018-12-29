1762c1b91c2aba907a922c93db03331ecd3af8e4
danailstratirv <stratyd@abv.bg>
2018-12-30 00:14:11 +0200

    Smallest of Three Numbers

---
+++ METHODS/Exercise 1 Smallest of Three Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Exercise_1_Smallest_of_Three_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                numbers.Add(n);
+            }
+            Console.WriteLine(numbers.Min());
+        }
+    }
+}
