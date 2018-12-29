3cf282c7795c470ff2a4eab7cd2ed85519cc842c
danailstratirv <stratyd@abv.bg>
2018-12-30 01:53:03 +0200

    Most frequent number

---
+++ ARRAYS/Exercise 8 Most frequent number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_8_Most_frequent_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int [] counters = new int [numbers.Length];
+            int counterMax = 0;
+            int maxNumber = 0; 
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        counters[i]++;
+
+                        if (counters[i] > counterMax)
+                        {
+                            counterMax = counters[i];
+                            maxNumber = numbers[i];
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(maxNumber);
+        }
+    }
+}
