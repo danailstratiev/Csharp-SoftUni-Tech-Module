420e2fa0acd19ff4f492c19031b99566d4e0e47e
danailstratirv <stratyd@abv.bg>
2018-12-30 00:17:12 +0200

    Vowels Count

---
+++ METHODS/Exercise 2 Vowels Count/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Exercise_2_Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine().ToLower();
+            int sum = 0;
+
+            foreach (var item in word)
+            {
+                switch (item)
+                {
+                    case 'a':
+                    case 'e':
+                    case 'o':
+                    case 'u':
+                    case 'y':
+                    case 'i':
+                        sum++;
+                        break;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
