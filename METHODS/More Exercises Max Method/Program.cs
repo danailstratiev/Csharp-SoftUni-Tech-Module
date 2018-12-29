e18cc92ae1cd80e0555e33d88b30d7e64a1c1a08
danailstratirv <stratyd@abv.bg>
2018-12-30 00:31:38 +0200

    Max Method

---
+++ METHODS/More Exercises Max Method/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _4._2_Max_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetMax(first,second,third));
+        }
+        static int GetMax (int first,int second,int third)
+        {
+            var biggestNumber1 = Math.Max(first, second);
+            var biggestofThree = Math.Max(biggestNumber1, third);
+            return biggestofThree;
+        }
+    }
+}
