01c964e24d7fee46d663f875beec0730c0633967
danailstratirv <stratyd@abv.bg>
2018-12-29 22:15:36 +0200

    Rectangle Area Task 1.2

---
+++ BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/1.2 Rectangle Area/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1._2_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double result = a * b;
+
+            Console.WriteLine($"{result:F2}");
+        }
+    }
+}
