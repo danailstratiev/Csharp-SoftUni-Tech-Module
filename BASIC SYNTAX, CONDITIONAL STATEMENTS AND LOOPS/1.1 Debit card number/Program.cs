ca4255a26b3878bde825562f7caf2c429b2522a5
danailstratirv <stratyd@abv.bg>
2018-12-29 22:12:47 +0200

    Exercises from the first topics in Tech Module

---
+++ BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/1.1 Debit card number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1._1_Debit_card_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstnum = int.Parse(Console.ReadLine());
+            int secondnum = int.Parse(Console.ReadLine());
+            int thirdnum = int.Parse(Console.ReadLine());
+            int forthdnum = int.Parse(Console.ReadLine());
+
+            Console.Write($"{firstnum:D4} {secondnum:D4} {thirdnum} {forthdnum:D4} ");
+
+        }
+    }
+}
