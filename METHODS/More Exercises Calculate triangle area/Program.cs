00262c6a04ee104ef9abe4f7cdaacabff2c51a2f
danailstratirv <stratyd@abv.bg>
2018-12-30 00:52:31 +0200

    Calculate triangle area

---
+++ METHODS/More Exercises Calculate triangle area/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _4._6_Lab_Calculate_triangle_area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sideA = double.Parse(Console.ReadLine());
+            double sideHa = double.Parse(Console.ReadLine());
+            var result = FindArea(sideA, sideHa);
+            Console.WriteLine(result);
+
+        }
+
+        static double FindArea (double sideA, double sideHa)
+        {
+            return sideA * sideHa / 2;
+        }
+    }
+}
