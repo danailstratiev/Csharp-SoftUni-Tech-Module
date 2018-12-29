bb6fe7402d725470024231a22a837daf196588da
danailstratirv <stratyd@abv.bg>
2018-12-30 01:22:28 +0200

    Factorial

---
+++ METHODS/More Exercises Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace _4._13_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Factorial(n);
+        }
+        static void Factorial (int n)
+        {
+           BigInteger factorialofN = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+             factorialofN *= i;
+            }
+            Console.WriteLine(factorialofN);
+        }
+    }
+}
