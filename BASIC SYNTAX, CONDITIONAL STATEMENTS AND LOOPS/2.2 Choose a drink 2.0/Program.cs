3ea36732617cde544a6f1a133b06bb54626ada15
danailstratirv <stratyd@abv.bg>
2018-12-29 22:35:51 +0200

    Choose a drink 2.0

---
+++ BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/2.2 Choose a drink 2.0/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _2._2_Choose_a_drink_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string profession = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            double totalprise = 0;
+
+            if (profession == "Athlete")
+            {
+                totalprise = quantity * 0.7;
+            }
+            else if (profession == "Businessman")
+            {
+                totalprise = quantity * 1.00;
+
+            }
+            else if (profession == "SoftUni Student")
+            {
+                totalprise = quantity * 1.7;
+            }
+            else
+            {
+                totalprise = quantity * 1.2;
+            }
+            Console.WriteLine($"The {profession} has to pay {totalprise:F2}.");
+        }
+    }
+}
