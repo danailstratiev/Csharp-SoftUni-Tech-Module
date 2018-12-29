03f7ff556f7366b12b59e8a21bf2e026d2b07357
danailstratirv <stratyd@abv.bg>
2018-12-30 01:42:57 +0200

    Fold and Sum

---
+++ ARRAYS/Exercise 3 Fold and Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_3_Fold_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int k = numbers.Length / 4;
+            int[] upper = new int[numbers.Length / 2];
+            int[] lower = new int[numbers.Length / 2];
+
+            for (int i = 0; i < k*2; i++)
+            {
+                lower[i] = numbers[k + i];
+            }
+            for (int i = 0; i < k; i++)
+            {
+                upper[i] = numbers[k - 1 - i];
+                upper[k + i] = numbers[4 * k - 1 - i];
+            }
+            int[] sum = new int[k * 2];
+
+            for (int i = 0; i < k*2; i++)
+            {
+                sum[i] = upper[i] + lower[i];
+            }
+            Console.WriteLine(string.Join(" ",sum));
+        }
+    }
+}
