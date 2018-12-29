8da2e07836ad8b51de9e5c2ea4a33c7d2d52970b
danailstratirv <stratyd@abv.bg>
2018-12-29 23:00:55 +0200

    Sum digits

---
+++ DATA TYPES AND VARIABLES/TechFun Data types p01 Sum digits/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+
+namespace TechFun_Data_types_p01_Sum_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (number>0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
