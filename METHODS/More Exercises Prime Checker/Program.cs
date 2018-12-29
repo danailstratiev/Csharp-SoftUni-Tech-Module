8696a377fa915eaf537141e6126834634ad8ae17
danailstratirv <stratyd@abv.bg>
2018-12-30 01:04:05 +0200

    Prime Checker

---
+++ METHODS/More Exercises Prime Checker/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _4._6_Prime_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long number = long.Parse(Console.ReadLine());
+            IsPrime(number);
+        }
+        static void IsPrime(long number)
+        {
+            bool isPrime = true;
+
+            for (long i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }               
+            }
+            Console.WriteLine($"{isPrime}");
+        }
+
+    }
+}
