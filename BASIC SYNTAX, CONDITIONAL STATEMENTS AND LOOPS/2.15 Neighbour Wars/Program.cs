0d26b55c7ed0390be04f5739cb89bf40a97efdaf
danailstratirv <stratyd@abv.bg>
2018-12-29 22:56:37 +0200

    Neighbour wars

---
+++ BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/2.15 Neighbour Wars/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _2._15_Neighbour_Wars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int damagePesho = int.Parse(Console.ReadLine());
+            int damageGosho = int.Parse(Console.ReadLine());
+            int peshoHealth = 100;
+            int goshoHealth = 100;
+            int round = 0;
+
+            while (peshoHealth > 0 && goshoHealth > 0)
+            {
+                round++ ;
+                if (round % 2 == 1)
+                {
+                    goshoHealth -= damagePesho;
+                    if (goshoHealth > 0)
+                    {
+                        Console.WriteLine($"Pesho used Roundhouse kick and reduced Gosho to {goshoHealth} health.");
+                    }
+                }
+                else
+                {
+                    peshoHealth -= damageGosho;
+                    if (peshoHealth > 0)
+                    {
+                        Console.WriteLine($"Gosho used Thunderous fist and reduced Pesho to {peshoHealth} health.");
+                    }
+                }
+                if (round % 3 == 0 && peshoHealth > 0 && goshoHealth > 0)
+                {
+                    peshoHealth += 10;
+                    goshoHealth += 10;
+                }
+            }
+            if (round % 2 == 0)
+            {
+                Console.WriteLine($"Gosho won in {round}th round.");
+            }
+            else
+            {
+                Console.WriteLine($"Pesho won in {round}th round.");
+
+            }
+
+        }
+    }
+}
