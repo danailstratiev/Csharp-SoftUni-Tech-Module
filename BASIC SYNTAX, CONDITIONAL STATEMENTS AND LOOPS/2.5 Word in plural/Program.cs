933a3817cb21a2bd19628eddc4af02d887002c39
danailstratirv <stratyd@abv.bg>
2018-12-29 22:40:39 +0200

    Word in plural

---
+++ BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/2.5 Word in plural/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _2._5_Word_in_plural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            if (word.EndsWith('y'))
+            {
+                word = word.Remove(word.Length-1);
+                word = word + "ies";
+            }
+            else if (word.EndsWith('z') || word.EndsWith('s') || word.EndsWith('o') || word.EndsWith('x') ||
+                word.EndsWith("ch") || word.EndsWith("sh"))
+            {
+                word = word + "es";
+            }
+            else
+            {
+                word = word + "s";
+            }
+            Console.WriteLine($"{word}");
+        }
+    }
+}
