c838631d593d7b8f719a8692328d7b06eee49da6
danailstratirv <stratyd@abv.bg>
2018-12-29 22:47:23 +0200

    Count the integers

---
+++ BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/2.9 Count the integers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _2._9_Count_the_integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+
+            try
+            {
+                while (true)
+                {
+                    int number = int.Parse(Console.ReadLine());
+                    counter++;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"{counter}");
+            }
+        }
+    }
+}
