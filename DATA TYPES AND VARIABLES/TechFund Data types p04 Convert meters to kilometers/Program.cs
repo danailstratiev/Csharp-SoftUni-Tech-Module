12714890061851ad7b71510fa5ac1f8f4ec84376
danailstratirv <stratyd@abv.bg>
2018-12-29 23:06:38 +0200

    Convert meters to kilometers

---
+++ DATA TYPES AND VARIABLES/TechFund Data types p04 Convert meters to kilometers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TechFund_Data_types_p04_Convert_meters_to_kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+            double kilometers = meters * 0.001;
+
+            Console.WriteLine($"{kilometers:F2}");
+        }
+    }
+}
