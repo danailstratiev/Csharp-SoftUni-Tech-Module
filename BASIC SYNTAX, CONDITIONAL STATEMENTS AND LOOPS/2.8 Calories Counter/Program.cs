5527ed92ca31100ba0beccf6e1b92b9883f5cc30
danailstratirv <stratyd@abv.bg>
2018-12-29 22:46:05 +0200

    Calories Counter

---
+++ BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/2.8 Calories Counter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _2._8_Calories_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string ingredient = Console.ReadLine().ToLower();
+                
+                if (ingredient == "cheese")
+                {
+                    counter += 500;
+                }
+               else if (ingredient == "tomato sauce")
+                {
+                    counter += 150;
+                }
+               else if (ingredient == "salami")
+                {
+                    counter += 600;
+                }
+               else if (ingredient == "pepper")
+                {
+                    counter += 50;
+                }
+            }
+            Console.WriteLine($"Total calories: {counter}");
+        }
+    }
+}
