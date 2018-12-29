f6142253cc470cf86b6d61301b43084605426cd4
danailstratirv <stratyd@abv.bg>
2018-12-29 23:04:02 +0200

    Chars to string

---
+++ DATA TYPES AND VARIABLES/TechFund Data types p02 Chars to string/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TechFund_Data_types_p02_Chars_to_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol1 = char.Parse(Console.ReadLine());
+            char symbol2 = char.Parse(Console.ReadLine());
+            char symbol3 = char.Parse(Console.ReadLine());
+            char[] symbols = new char[3];
+            symbols[0] = symbol1;
+            symbols[1] = symbol2;
+            symbols[2] = symbol3;
+            
+
+
+            Console.WriteLine(string.Join("",symbols));
+        }
+    }
+}
