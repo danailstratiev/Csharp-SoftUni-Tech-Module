dc8d3cfa3e8c212eaec2c1f135034b3006f0e0e6
danailstratirv <stratyd@abv.bg>
2018-12-29 23:08:32 +0200

    Reverse chars

---
+++ DATA TYPES AND VARIABLES/TechFund Data types p06 Reverse chars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace TechFund_Data_types_p06_Reverse_chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol1 = char.Parse(Console.ReadLine());
+            char symbol2 = char.Parse(Console.ReadLine());
+            char symbol3 = char.Parse(Console.ReadLine());
+            List<char> symbols = new List <char>();
+            symbols.Add(symbol1);
+            symbols.Add(symbol2);
+            symbols.Add(symbol3);
+            symbols.Reverse();
+
+
+
+            Console.WriteLine(string.Join(" ", symbols));
+        }
+    }
+}
