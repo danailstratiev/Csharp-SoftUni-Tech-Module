90c14a2e1e58b0970d21080f6ae7ceb3780f1717
danailstratirv <stratyd@abv.bg>
2018-12-30 00:58:57 +0200

    Multiplicating Odds by even numbers

---
+++ METHODS/More Exercises Multiply Odds by even digits/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _4._9_Lab_Multiply_Odds_by_even_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            var absNumber = Math.Abs(number);
+
+            var result = GetMultipleOfEvensAndOdds(absNumber);
+
+            Console.WriteLine(result);
+        }
+        static int GetMultipleOfEvensAndOdds(int number)
+        {
+            var evenSum = GetSumOfEvenDigits(number);
+            var oddSum = GetSumOfOddDigits(number);
+
+            return evenSum * oddSum;
+        }
+        static int GetSumOfEvenDigits(int number)
+        {
+
+            int sum = 0;
+
+            while (number > 0)
+            {
+                var currentDigit = number % 10;
+                number /= 10;
+
+                if (currentDigit % 2 == 0)
+                {
+                    sum += currentDigit;
+                }
+            }
+            return sum;
+        }
+        static int GetSumOfOddDigits(int number)
+        {
+            int sum = 0;
+
+            while (number > 0)
+            {
+                var currentDigit = number % 10;
+                number /= 10;
+
+                if (currentDigit % 2 == 1)
+                {
+                    sum += currentDigit;
+                }
+            }
+                return sum;
+
+        }
+
+
+    }
+}
