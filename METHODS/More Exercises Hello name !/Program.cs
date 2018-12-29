74a54b01e7f4c6cd4dba584f62e52e2826eabfe6
danailstratirv <stratyd@abv.bg>
2018-12-30 00:29:52 +0200

    Hello name !

---
+++ METHODS/More Exercises Hello name !/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4._1_Hello_name__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            HiName(name);
+        }
+        static void HiName (string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
