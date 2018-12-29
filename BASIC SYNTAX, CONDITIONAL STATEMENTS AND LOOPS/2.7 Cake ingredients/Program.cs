a8fc89cadbe5c74c5eb3a9379168c8da032fc9f8
danailstratirv <stratyd@abv.bg>
2018-12-29 22:44:56 +0200

    Cake ingredients

---
+++ BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/2.7 Cake ingredients/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _2._7_Cake_ingredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ingredient = Console.ReadLine();
+            int counter = 0;
+
+            while (ingredient != "Bake!")
+            {
+                counter++;
+                Console.WriteLine($"Adding ingredient {ingredient}.");
+                ingredient = Console.ReadLine();
+               
+                
+            }
+            Console.WriteLine($"Preparing cake with {counter} ingredients.");
+
+        }
+    }
+}
