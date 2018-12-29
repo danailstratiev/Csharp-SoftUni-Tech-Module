108d15e70fb27ab5d9bd6d7c1f3e13eda508ea83
danailstratirv <stratyd@abv.bg>
2018-12-30 01:53:49 +0200

    Index of Letters

---
+++ ARRAYS/Exercise 9 Index of Letters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise_9_Index_of_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            char[] alphabet = new char [26];
+            int indecCounter = 0;
+
+            for (char letter = 'a'; letter <= 'z'; letter++)
+            {
+                alphabet[indecCounter] = letter;
+                indecCounter++;
+            }
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                char currentLetter = word[i];
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    if (currentLetter == alphabet[j])
+                    {
+                        Console.WriteLine($"{currentLetter} -> {j}");
+                    }
+                }
+            }
+        }
+    }
+}
