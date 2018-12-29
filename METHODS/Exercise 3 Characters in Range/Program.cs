a1dedf1240bde5085208dbd618d3df29d64d85d2
danailstratirv <stratyd@abv.bg>
2018-12-30 00:19:04 +0200

    Characters in Range

---
+++ METHODS/Exercise 3 Characters in Range/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise_3_Characters_in_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol1 = char.Parse(Console.ReadLine());
+            char symbol2 = char.Parse(Console.ReadLine());
+
+            for (int i = Math.Min((int)symbol1, (int)symbol2) + 1; i < Math.Max((int)symbol1, (int)symbol2); i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+    }
+}
