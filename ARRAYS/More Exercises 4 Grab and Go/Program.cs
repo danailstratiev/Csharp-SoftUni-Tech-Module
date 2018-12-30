c92378f8b0e9d76b79f252634c7722f1981388d1
danailstratirv <stratyd@abv.bg>
2018-12-30 18:22:13 +0200

    Grab and Go

---
+++ ARRAYS/More Exercises 4 Grab and Go/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace More_Exercises_4_Grab_and_Go
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int n = int.Parse(Console.ReadLine());
+            int index = -1;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == n)
+                {
+                    index = i;
+                }
+            }
+            if (!numbers.Contains(n))
+            {
+                Console.WriteLine("No occurrences were found!");
+            }
+            else
+            {
+            Console.WriteLine(numbers.Take(index).Sum());                        
+            }
+        }
+    }
+}
