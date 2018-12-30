4948aea84b6b48eebdd6501353f4a46ea1582ee8
danailstratirv <stratyd@abv.bg>
2018-12-30 18:28:14 +0200

    Jump Around

---
+++ ARRAYS/More Exercises 9 Jump Around/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace More_Exercises_9_Jump_Around
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int currentValue = 0;
+            int currentIndex = 0;            
+            int sum = 0;
+            bool stop = false;
+            int position = 0;
+            currentIndex = 0;
+            currentValue = numbers[currentIndex];
+            sum = 0;
+
+
+            while (stop == false)
+            {                
+
+                if (currentValue >= numbers.Length && currentIndex - currentValue < 0)
+                {
+                    if (sum == 0)
+                    {
+                        sum = currentValue;
+                    }
+                    Console.WriteLine(sum);
+                    stop = true;
+                    break;
+                }
+                else if (currentValue >= numbers.Length)
+                {
+                    position = currentIndex - currentValue;
+
+                    if (position >= 0)
+                    {
+                        currentIndex = position;
+                        currentValue = numbers[currentIndex];
+                        sum += currentValue;
+                        stop = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine(sum);
+                        stop = true;
+                        break;
+                    }
+                }
+                else if (currentValue < 0)
+                {
+                    position = currentIndex + currentValue;
+
+                    if (position < numbers.Length)
+                    {
+                        currentIndex = position;
+                        currentValue = numbers[currentIndex];
+                        sum += currentValue;
+                        stop = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine(sum);
+                        stop = true;
+                        break;
+                    }
+                }
+                else
+                {
+                    if (currentIndex < 0)
+                    {
+                        Console.WriteLine(sum);
+                        stop = true;
+                        break;
+                    }
+                    currentValue = numbers[currentIndex];
+                    sum += currentValue;
+                    if (currentValue + currentIndex >= numbers.Length)
+                    {
+                        currentIndex -= currentValue;
+                    }                    
+                    else
+                    {
+                     currentIndex += currentValue;          
+
+                    }
+                    stop = false;
+                }
+            }
+        }
+    }
+}
