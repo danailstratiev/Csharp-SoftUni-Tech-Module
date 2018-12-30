1cb812ed71a432cdcb8d58520b4c4c2737b5a47d
danailstratirv <stratyd@abv.bg>
2018-12-30 02:30:10 +0200

    Array Rotation

---
+++ ARRAYS/TechFund Arrays p04 Array Rotation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace TechFund_Arrays_p04_Array_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers = Rotated(numbers);
+            }
+            Console.WriteLine(string.Join(" ",numbers));
+
+        }static int[]Rotated (int[]numbers)
+        {
+            int[] rotated = new int[numbers.Length];
+            rotated[rotated.Length - 1] = numbers[0];
+
+            for (int i = 0; i < rotated.Length-1; i++)
+            {
+                rotated[i] = numbers[i+1];
+            }
+            return rotated;
+        }
+    }
+}
