5b9b083f7b25a4dfc580879f5d50dbe12f44c9fc
danailstratirv <stratyd@abv.bg>
2018-12-30 12:52:35 +0200

    Sum reverse numbers version 2

---
+++ LISTS ARRAYS ADVANCED/Lists Exercise Sum Reverse Numbers Version 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Lists_Exercise_6._1_Sum_Reverse_Numbers_Version_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                List<char> num = numbers[i].ToString().ToList();
+                num.Reverse();
+                sum += int.Parse(string.Join("",num));
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
