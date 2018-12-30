c35648b662a08731862df90958de5bdcf2b5e81d
danailstratirv <stratyd@abv.bg>
2018-12-30 12:24:35 +0200

    Rounding numbers

---
+++ ARRAYS/Lab Problem Rounding Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab_Problem_Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "-5.01 -1.599 -2.5 -1.50 0";
+            double[] numbers = text.Split(' ').Select(double.Parse).ToArray();
+
+            foreach (var number in numbers)
+            {
+                var roundednumber = Math.Round(number, MidpointRounding.AwayFromZero);
+                Console.WriteLine($"{number} => {roundednumber}");
+            }
+        }
+    }
+}
