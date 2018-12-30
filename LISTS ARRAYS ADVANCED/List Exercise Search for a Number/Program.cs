ab688427e1221f6ae725dedcfe0a01d4a3677f39
danailstratirv <stratyd@abv.bg>
2018-12-30 12:27:57 +0200

    Search for a Number

---
+++ LISTS ARRAYS ADVANCED/List Exercise Search for a Number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace List_Exercise_3_Search_for_a_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            int[] conditions = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            List<int> newNumbers = new List<int>();
+
+            for (int i = 0; i < conditions[0]; i++)
+            {
+                newNumbers.Add(numbers[i]);
+            }
+            int secondTask = conditions[1];
+            newNumbers.RemoveRange(0,secondTask);
+
+            if (newNumbers.Contains(conditions[2]))
+            {
+                Console.WriteLine("YES!");
+            }
+            else
+            {
+                Console.WriteLine("NO!");
+            }
+
+        }
+    }
+}
