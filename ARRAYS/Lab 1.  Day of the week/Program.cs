dbb924fbed31031aa53a983bab644fb741464ec0
danailstratirv <stratyd@abv.bg>
2018-12-30 02:07:30 +0200

    Day of the week

---
+++ ARRAYS/Lab 1.  Day of the week/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab_1.__Day_of_the_week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = {"Monday",
+             "Tuesday",
+             "Wednesday",
+             "Thursday",
+             "Friday",
+             "Saturday",
+             "Sunday"
+             };
+            int day = int.Parse(Console.ReadLine());
+
+            if (day < 1 || day >7)
+            {
+                Console.WriteLine("Invalid day");
+                return;
+            }
+            string daysInEnglish = days[day-1];
+
+            Console.WriteLine(daysInEnglish);
+        }
+    }
+}
