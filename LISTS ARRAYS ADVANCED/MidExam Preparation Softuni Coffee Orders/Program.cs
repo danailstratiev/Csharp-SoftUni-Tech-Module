db3a3f243483d402179d518fd71f222beca6dfec
danailstratirv <stratyd@abv.bg>
2018-12-30 13:18:08 +0200

    SoftUni Coffee Orders

---
+++ LISTS ARRAYS ADVANCED/MidExam Preparation Softuni Coffee Orders/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace MidExam_Preparation_Softuni_Coffee_Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOrders = int.Parse(Console.ReadLine());
+            decimal totalPrice = 0;
+
+            for (int i = 0; i < numberOfOrders; i++)
+            {
+                decimal price = decimal.Parse(Console.ReadLine());
+                DateTime date = DateTime.ParseExact(Console.ReadLine(), "d/M/yyyy", CultureInfo.InvariantCulture);
+                long capsules = long.Parse(Console.ReadLine());
+
+                int daysInMonth = DateTime.DaysInMonth(date.Year, date.Month);
+
+                decimal orderPrice = (daysInMonth * capsules) * price;
+
+                totalPrice += orderPrice;
+
+                Console.WriteLine($"The price for the coffee is: ${orderPrice:F2}");
+            }
+            Console.WriteLine($"Total: ${totalPrice:F2}");
+        }
+    }
+}
