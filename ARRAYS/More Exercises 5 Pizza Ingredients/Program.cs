3a8977a10ef566f0e7850166194b81b9679e300e
danailstratirv <stratyd@abv.bg>
2018-12-30 18:24:05 +0200

    Pizza Ingredients

---
+++ ARRAYS/More Exercises 5 Pizza Ingredients/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace More_Exercises_5_Pizza_Ingredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> ingredients = Console.ReadLine().Split(" ").ToList();
+            int n = int.Parse(Console.ReadLine());
+            List<string> pizza = new List<string>();
+          
+                for (int i = 0; i < ingredients.Count; i++)
+                {
+                    if (ingredients[i].Length == n)
+                    {
+                    pizza.Add(ingredients[i]);
+                        Console.WriteLine($"Adding {ingredients[i]}.");
+                    if (pizza.Count >= 10)
+                    {
+                        break;
+                    }
+                    }
+                }
+            Console.WriteLine($"Made pizza with total of {pizza.Count} ingredients.");
+            Console.WriteLine($"The ingredients are: {string.Join(", ",pizza)}.");            
+
+        }
+    }
+}
