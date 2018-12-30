a57d67a593e667f92f433e41cee9375d0269d129
danailstratirv <stratyd@abv.bg>
2018-12-30 03:16:59 +0200

    Train

---
+++ LISTS ARRAYS ADVANCED/TechFund Lists Exercises 1 Train/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace TechFund_p01_Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> wagons = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int wagon = int.Parse(Console.ReadLine());
+                wagons.Add(wagon);
+            }
+            Console.WriteLine(string.Join(" ",wagons));
+            int sum = wagons.Sum();
+            Console.WriteLine(sum);
+        }
+    }
+}
