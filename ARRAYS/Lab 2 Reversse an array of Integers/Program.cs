3cdc515cacd68bde48e11d47b38bc6b7f3e60b11
danailstratirv <stratyd@abv.bg>
2018-12-30 02:09:42 +0200

    Reverse an array of Integers

---
+++ ARRAYS/Lab 2 Reversse an array of Integers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab_2_Reversse_an_array_of_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int[] numbers = new int[number];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            // може и по следния начин:
+            //int[] reversednumbers = numbers.Reverse().ToArray;
+            //което ще създаде нов масив
+        }
+    }
+}
