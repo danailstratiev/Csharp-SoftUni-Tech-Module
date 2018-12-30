66e7690db5c81b1ff253042bc19cde42686db7df
danailstratirv <stratyd@abv.bg>
2018-12-30 13:28:32 +0200

    Command interpreter

---
+++ LISTS ARRAYS ADVANCED/MidExam Preparation Command Interpreter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace MidExam_Preparation_Command_Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int startIndex = 2;
+            int count = 4;
+
+            numbers.Reverse(startIndex, count);
+
+        }
+    }
+}
