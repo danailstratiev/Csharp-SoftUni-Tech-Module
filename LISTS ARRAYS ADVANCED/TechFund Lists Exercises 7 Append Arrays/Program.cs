8b5c85322c16a3b38a45e6323f6b73b81ea6b594
danailstratirv <stratyd@abv.bg>
2018-12-30 03:11:35 +0200

    Append Arrays

---
+++ LISTS ARRAYS ADVANCED/TechFund Lists Exercises 7 Append Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace TechFund_Lists_Exercises_7_Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> collection = Console.ReadLine().Split('|').ToList();
+            collection.Reverse();
+            var appened = new List<string>();
+
+            for (int i = 0; i < collection.Count; i++)
+            {
+                string[] helper = collection[i].Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                foreach (var item in helper)
+                {
+                    appened.Add(item);
+                }
+            }
+            Console.WriteLine(string.Join(" ", appened));
+            
+            
+
+        }
+    }
+}
