c9a2aaab046f7a985fcd234457b8dbd463abf20d
danailstratirv <stratyd@abv.bg>
2018-12-30 13:19:15 +0200

    SoftUni Coffee Orders

---
+++ LISTS ARRAYS ADVANCED/MidExam Preparation Snowballs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+
+
+namespace MidExam_Preparation_Snowballs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int snowballs = int.Parse(Console.ReadLine());
+            BigInteger snowballValue = 0;
+            BigInteger bestBall = 0;
+            var bestQuality = 0;
+            var bestTime = 0;
+            var bestSnow = 0;
+
+            for (int i = 0; i < snowballs; i++)
+            {
+                var snowballSnow = int.Parse(Console.ReadLine());
+                var snowballTime = int.Parse(Console.ReadLine());
+                var snowballQuality = int.Parse(Console.ReadLine());
+
+                snowballValue = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality) ;
+
+                if (snowballValue > bestBall)
+                {
+                    bestBall = snowballValue;
+                    bestQuality = snowballQuality;
+                    bestTime = snowballTime;
+                    bestSnow = snowballSnow;
+                }
+            }
+            Console.WriteLine($"{bestSnow} : {bestTime} = {bestBall} ({bestQuality})");
+        }
+    }
+}
