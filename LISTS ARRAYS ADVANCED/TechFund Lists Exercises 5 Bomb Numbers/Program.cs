bcb007fd0ac6325d50639a66025849daf82cf028
danailstratirv <stratyd@abv.bg>
2018-12-30 03:09:41 +0200

    Bomb Numbers

---
+++ LISTS ARRAYS ADVANCED/TechFund Lists Exercises 5 Bomb Numbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace TechFund_Lists_Exercises_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int[] commands = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int bombNumber = commands[0];
+            int bombPower = commands[1];
+
+            while (numbers.Contains(bombNumber))
+            {
+                int position = numbers.IndexOf(bombNumber);
+
+                if (0 > position - bombPower && position + bombPower >= numbers.Count)
+                {
+                    break;
+                }
+                else if (position - bombPower < 0)
+                {
+                    numbers.RemoveRange(0, position - 0 + 1 + bombPower);
+                }
+                else if (position + bombPower >= numbers.Count)
+                {
+                    numbers.RemoveRange(position - bombPower, bombPower + 1 + numbers.Count -1 - position);
+                }
+                else
+                {
+                    numbers.RemoveRange(position - bombPower, bombPower * 2 + 1);
+                }
+            }
+            Console.WriteLine(numbers.Sum());            
+        }
+    }
+}
