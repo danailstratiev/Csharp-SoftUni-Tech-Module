073d042ff9c666c21dfd076d72bb7539a39e7337
danailstratirv <stratyd@abv.bg>
2018-12-30 13:07:09 +0200

    Demoexam task 3

---
+++ LISTS ARRAYS ADVANCED/DemoExam Task 3 Baking Factory/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace DemoExam_Task_3_Baking_Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string batch = Console.ReadLine();
+            List<int> badBatch = new List<int>();
+            badBatch.Add(-1001);
+
+            while (batch != "Bake It!")
+            {
+                var batchQuality = batch.Split("#").Select(int.Parse).ToList();
+                if (batchQuality.Count <= 10)
+                {                
+                if (batchQuality.Sum() > badBatch.Sum() ||
+                    (batchQuality.Sum() == badBatch.Sum() && batchQuality.Average() > badBatch.Average()) ||
+                  (batchQuality.Sum() == badBatch.Sum() && batchQuality.Average() == badBatch.Average() && batchQuality.Count < badBatch.Count))
+                {
+                    badBatch = batchQuality;
+                }                
+                }
+             batch = Console.ReadLine();
+            }
+            Console.WriteLine($"Best Batch quality: {badBatch.Sum()}");
+            Console.WriteLine(string.Join(" ",badBatch));
+        }
+    }
+}
