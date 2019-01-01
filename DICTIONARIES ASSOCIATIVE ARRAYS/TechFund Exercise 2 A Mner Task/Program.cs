36c1dbafedf9fb839a1f7571db8c0e2348dbee7f
danailstratirv <stratyd@abv.bg>
2019-01-01 23:17:21 +0200

    A Miner Task

---
+++ DICTIONARIES ASSOCIATIVE ARRAYS/TechFund Exercise 2 A Mner Task/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace TechFund_Exercise_2_A_Mner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+
+            while (true)
+            {
+                string resource = Console.ReadLine();
+
+                if (resource == "stop")
+                {
+                    break;
+                }
+               int quantity = int.Parse(Console.ReadLine());
+
+                if (!resources.ContainsKey(resource))
+                {
+                    resources.Add(resource, quantity);
+                }
+                else
+                {
+                    resources[resource] += quantity;
+                }
+            }
+            foreach (var kvp in resources)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
