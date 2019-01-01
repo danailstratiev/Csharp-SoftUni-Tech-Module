efff4a255976b1b760236dd60eae4496673aae5a
danailstratirv <stratyd@abv.bg>
2019-01-01 23:16:17 +0200

    Count Chars in a String

---
+++ DICTIONARIES ASSOCIATIVE ARRAYS/TechFund Exercise 1 Count Chars in a String/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace TechFund_Exercise_1_Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Dictionary<char, int> charCounter = new Dictionary<char, int>();
+
+            foreach (var letter in text)
+            {
+                if (letter != ' ')
+                {
+                    if (!charCounter.ContainsKey(letter))
+                    {
+                        charCounter.Add(letter, 1);
+                    }
+                    else
+                    {
+                        charCounter[letter]++;
+                    }
+                }
+            }
+            foreach (var kvp in charCounter)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+
+        }
+    }
+}
