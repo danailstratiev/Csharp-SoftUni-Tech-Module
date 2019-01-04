0cdf15c7c6d71588d460f092380d9cc6dc02ff7c
danailstratirv <stratyd@abv.bg>
2019-01-04 21:40:22 +0200

    Sort Times

---
+++ DICTIONARIES ASSOCIATIVE ARRAYS/More Exercises 1 Sort Times/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace More_Exercises_1_Sort_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> times = Console.ReadLine().Split(" ").ToList();
+            times.Sort();
+
+            Console.WriteLine(string.Join(", ",times));
+        }
+    }
+}
