fb94f7fe2873ea16e1a08d46b1ec72c3737d2374
danailstratirv <stratyd@abv.bg>
2019-01-08 19:06:41 +0200

    Concert

---
+++ DICTIONARIES ASSOCIATIVE ARRAYS/Practical Exam Task 1 Concert/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Exam_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var bandNameAndMembers = new Dictionary<string, List<string>>();
+            var bandPlayTime = new Dictionary<string, int>();
+
+            while (input != "start of concert")
+            {
+                var typeOfAction = input.Split("; ").ToArray();
+
+                if (typeOfAction[0] == "Play")
+                {
+                    string bandName = typeOfAction[1];
+                    int bandConcertTime = int.Parse(typeOfAction[2]);
+
+                    if (!bandPlayTime.ContainsKey(bandName))
+                    {
+                        bandPlayTime[bandName] = bandConcertTime;
+                    }
+                    else
+                    {
+                        bandPlayTime[bandName] += bandConcertTime;
+                    }
+                }
+                else if (typeOfAction[0] == "Add")
+                {
+                    string bandName = typeOfAction[1];
+                    List<string> bandMembers = typeOfAction[2].Split(", ").ToList();
+
+                    if (!bandNameAndMembers.ContainsKey(bandName))
+                    {
+                        bandNameAndMembers[bandName] = bandMembers;
+                    }
+                    else
+                    {
+                        var rockOfAges = bandNameAndMembers[bandName];
+
+                        foreach (var performer in bandMembers)
+                        {
+                            if (!rockOfAges.Contains(performer))
+                            {
+                                rockOfAges.Add(performer);
+                            }
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            string timeForRockAndRoll = Console.ReadLine();
+            var totalPlayTime = bandPlayTime.Values.Sum();
+
+            Console.WriteLine($"Total time: {totalPlayTime}");
+            foreach (var concertTime in bandPlayTime.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{concertTime.Key} -> {concertTime.Value}");
+            }
+
+            Console.WriteLine(timeForRockAndRoll);
+            foreach (var player in bandNameAndMembers.Where(x => x.Key == timeForRockAndRoll))
+            {
+                var members = player.Value;
+                foreach (var member in members)
+                {
+                  Console.WriteLine($"=> {member}");
+                }
+            }
+        }
+    }
+}
