2289ff908488147a0b218192466e7762738f5460
danailstratirv <stratyd@abv.bg>
2019-01-11 17:05:23 +0200

    Randomize Words

---
+++ Objects and Classes/Lab Randomize Words/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab_2_Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine().Split();
+
+            var random = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                var ri = random.Next(0, words.Length);
+
+                var tempValue = words[i];
+                words[i] = words[ri];
+                words[ri] = tempValue;
+            }
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+        }
+    }
+}
