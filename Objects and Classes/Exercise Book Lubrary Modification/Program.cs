6312d398b657abcf39239ce0dd291e3980bff26b
danailstratirv <stratyd@abv.bg>
2019-01-12 00:48:44 +0200

    Book Library Modification

---
+++ Objects and Classes/Exercise Book Lubrary Modification/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Globalization;
+
+
+
+namespace Exercise_6_Book_Lubrary_Modification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Book> myBooks = new List<Book>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ").ToArray();
+                Book book = new Book();
+                book.Title = input[0];
+                book.Author = input[1];
+                book.Publisher = input[2];
+                book.ReleaseDate = DateTime.ParseExact(input[3], "dd.MM.yyyy", CultureInfo.InvariantCulture) ;
+                book.ISBN = input[4];
+                book.Price = double.Parse(input[5]);
+                myBooks.Add(book);
+            }
+            DateTime startDate = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+            Library myLibrary = new Library();
+            myLibrary.Books = myBooks;
+
+            foreach (var book in myLibrary.Books.Where(x => x.ReleaseDate > startDate).OrderBy(x => x.ReleaseDate).ThenBy(x => x.Title))
+            {
+                Console.WriteLine($"{book.Title} -> {book.ReleaseDate.ToString("dd.MM.yyyy")}");
+            }
+        }
+    }
+    // {title} {author} {publisher} {release date} {ISBN} {price}.
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string ISBN { get; set; }
+        public double Price { get; set; }
+    }
+    class Library
+    {
+        public List <Book> Books { get; set; }
+        public string Name { get; set; }
+    }
+}
