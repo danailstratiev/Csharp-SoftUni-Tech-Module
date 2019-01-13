2473f978b77fbc7190443106a76ec1f802a6e653
danailstratirv <stratyd@abv.bg>
2019-01-13 21:56:52 +0200

    Multiply Big Number

---
+++ Strings and Text Processing/TechFund Exercise 5 Multiply Big Number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+
+
+namespace TechFund_Exercise_5_Multiply_Big_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger firstNumber = BigInteger.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            BigInteger result = firstNumber * secondNumber;
+
+            Console.WriteLine(result);
+        }
+    }
+}
