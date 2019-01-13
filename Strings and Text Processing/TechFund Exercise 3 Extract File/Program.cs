c6c041198b3ae98cf0beea1ea46816a978c7c88a
danailstratirv <stratyd@abv.bg>
2019-01-13 21:55:18 +0200

    Extract File

---
+++ Strings and Text Processing/TechFund Exercise 3 Extract File/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace TechFund_Exercise_3_Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] someString = Console.ReadLine().Split('\\').ToArray();
+
+            string[] last = someString[someString.Length - 1].Split('.').ToArray();
+            string fileName = last[0];
+            string extension = last[1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
