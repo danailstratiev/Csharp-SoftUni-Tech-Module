a2e0c4b362b9e65966aecdc5e27ea2ddfafdc72b
danailstratirv <stratyd@abv.bg>
2019-01-13 21:56:06 +0200

    Caesar Cipher

---
+++ Strings and Text Processing/TechFund Exercise 4 Caesar Cipher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace TechFund_Exercise_4_Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = Console.ReadLine();
+            int transformed = 0;
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char symbol in phrase)
+            {
+                transformed = symbol + 3;
+                sb.Append((char)transformed);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
