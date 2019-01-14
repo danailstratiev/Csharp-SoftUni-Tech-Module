284711ce9071b69a36c321f50def33400bf90d45
danailstratirv <stratyd@abv.bg>
2019-01-14 21:24:16 +0200

    String Explosion

---
+++ Strings and Text Processing/TechFund Exercise 7 String Explosion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace TechFund_Exercise_7._1_String_Explosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string someString = Console.ReadLine();
+            int power = 0;
+
+            for (int i = 0; i < someString.Length; i++)
+            {
+                if (someString[i] == '>')
+                {
+                    power += int.Parse(someString[i+1].ToString());
+                    continue;
+                }
+                if ( power > 0)
+                {
+                    someString = someString.Remove(i, 1);
+                    i--;
+                    power--;
+                }
+            }
+            Console.WriteLine(someString);
+        }
+    }
+}
