475d3bf808ee3227e7be17706e4fa7c315c713c0
danailstratirv <stratyd@abv.bg>
2019-01-15 15:43:22 +0200

    Extract Emails

---
+++ Regular Expressions/Regex Exercise 1 Extract Emails/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
+
+
+namespace Regex_Exercise_1_Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string someString = Console.ReadLine();
+            string pattern = @"(^|(?<=\s))([a-z0-9]+)([_\.-]?[a-z0-9])*@([a-z0-9])+([-.][a-z0-9]+)*\.([a-z0-9]+)([-.][a-z-0-9]+)*";
+
+            MatchCollection matches = Regex.Matches(someString, pattern);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
