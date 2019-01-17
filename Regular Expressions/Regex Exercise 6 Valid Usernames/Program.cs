a5aa8f2c487f8d52098702e236d039c951f742ed
danailstratirv <stratyd@abv.bg>
2019-01-17 23:23:05 +0200

    Valid Usernames

---
+++ Regular Expressions/Regex Exercise 6 Valid Usernames/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace Regex_Exercise_6_Valid_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b([A-Za-z][\w]{2,24})\b";
+            string[] userNames = Console.ReadLine().Split(new char[] { ' ', '/', '\\', '(', ')' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            List<string> result = new List<string>();
+
+            foreach (var name in userNames)
+            {
+                if (Regex.IsMatch(name,pattern))
+                {
+                    result.Add(name);
+                }
+            }
+            int counter = 0;
+            int counterMax = 0;
+            int position = 0;
+
+            for (int i = 0; i < result.Count-1; i++)
+            {
+                counter = result[i].Length + result[i + 1].Length;
+
+                if (counter > counterMax)
+                {
+                    counterMax = counter;
+                    position = i;
+                }
+            }
+
+            Console.WriteLine(result[position]);
+            Console.WriteLine(result[position+1]);
+        }
+    }
+}
