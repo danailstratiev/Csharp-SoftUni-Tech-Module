ad755d34fb4897416f602978c8726f394afa77ac
danailstratirv <stratyd@abv.bg>
2019-01-19 21:14:04 +0200

    Censorship

---
+++ Regular Expressions/Strings and Regex Exercise 1 Censorship/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+
+namespace Strings_and_Regex_Exercise_1_Censorship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string keyword = Console.ReadLine();
+            string someString = Console.ReadLine();
+
+            string exchange = new string('*', keyword.Length);
+
+            someString = someString.Replace(keyword, exchange);
+
+            Console.WriteLine(someString);
+        }
+    }
+}
