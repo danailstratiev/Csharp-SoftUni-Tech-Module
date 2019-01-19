39d2aa97f973c531b61320a58d0ad5fe1a28009b
danailstratirv <stratyd@abv.bg>
2019-01-19 21:11:45 +0200

    Match Full Name

---
+++ Regular Expressions/Regex Lab 1 Match Full Name/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace Regex_Lab_1_Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string someString = Console.ReadLine();
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            MatchCollection matches = Regex.Matches(someString, pattern);
+
+            foreach (var match in matches)
+            {
+                Console.Write(match + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
