cc8b48d34f28f1d9378003fe6daf5b51f1c0f09b
danailstratirv <stratyd@abv.bg>
2019-04-25 10:25:33 +0300

    Calculator App

---
+++ Basic Web/Calculator/CalculatorApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using CalculatorApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+    public class HomeController : Controller
+    {
+       [HttpGet]
+        public IActionResult Index (decimal leftOperand, decimal rightOperand, string @operator, decimal result)
+        {
+            
+            Calculator calculator = new Calculator()
+            {
+                LeftOperand = leftOperand,
+                RightOperand = rightOperand,
+                Operator = @operator,
+                Result = result
+            };
+
+            return View (calculator);
+        }
+        [HttpPost]
+        public IActionResult Calculate(Calculator calculator)
+        {
+            calculator.CalculateResult();
+
+            if (calculator.RightOperand == 0 && calculator.Operator == "/")
+            {
+                TempData["Error"] = "Can not divide by 0 !";
+            }
+
+            return RedirectToAction("Index", calculator);
+        }
+    }
+}
